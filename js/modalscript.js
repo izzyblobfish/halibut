c3f6f58e5c4e9ccd9b291087bf54c9989f1f2b7d
izzyblobfish <izzyblobfish@outlook.com>
2024-01-14 15:02:01 -0800

    Update modalscript.js

--- js/modalscript.js
+++ js/modalscript.js
@@ -1,204 +1,101 @@
-//I dont claim to know how to write javascript
 
-const galleryItems = document.querySelectorAll('.entry');
-const modal = document.querySelector('.modal');
-const modalImage = document.querySelector('.modalImg');
+
+const images = document.querySelectorAll('.entry img');
+const entries = document.querySelectorAll('.entry')
+const lightbox = document.querySelector('.modal');
+const mainImg = document.querySelector('.modalImg');
+const arrLeft = document.querySelector('.prevBtn');
+const arrRight = document.querySelector('.nextBtn');
 const close = document.querySelector('.close');
-const prevBtn = document.querySelector('.prevBtn');
-const nextBtn = document.querySelector('.nextBtn');
-const modalText = document.querySelector('.modalTxt');
+const modalTxt = document.querySelector(".modalTxt");
 
-let currentIndex = 0;
+let imgIndex = 0;
 
 
-function openModal(index) {
-    modal.classList.add('appear');
-    showImage(index);
-}
+entries.forEach(img => {
+    img.addEventListener('click', e => {
 
-function showImage(index) {
-    index = (index + galleryItems.length) % galleryItems.length;
-    const imgSrc = galleryItems[index].querySelector('img').src;
-    const altText = galleryItems[index].querySelector('img').alt;
-    modalImage.src = imgSrc;
-    modalText.innerHTML = altText;
-    currentIndex = index;
-    
-}
-
-document.addEventListener('keydown', function(ev) {
-
-  if(ev.keyCode === 39){
-    showImage(currentIndex + 1);
-    console.log(currentIndex);
-
-    }
- 
-
-  else if(ev.keyCode === 37){
-    
-    showImage(currentIndex - 1);
-    console.log(currentIndex);
-
-  }
-  if (event.key === 'Escape') {
-    modal.classList.remove("appear");
-  }
- 
-});
-
-
-
-function clickableDiv() {
-    galleryItems.forEach((item, index) => {
-        item.addEventListener('click', () => openModal(index));
+        mainImg.src = e.target.src;
+        modalTxt.innerHTML = e.target.alt;
+        imgIndex = [...entries].indexOf(img);
+        mainImg.src = images[imgIndex].src;
+        modalTxt.innerHTML = images[imgIndex].alt;
+        lightbox.classList.add("appear");
+        
     });
-}
-
-
-close.addEventListener("click", () => {
-  modal.classList.remove("appear");
 });
-prevBtn.addEventListener('click', () => showImage(currentIndex - 1));
-nextBtn.addEventListener('click', () => showImage(currentIndex + 1));
-
-clickableDiv();
-
-
-
-
 
 /*
 
+images.forEach(img => {
+    img.addEventListener('click', e => {
 
-const images = document.querySelectorAll(".entry img"); 
+        mainImg.src = e.target.src;
+        modalTxt.innerHTML = e.target.alt;
+        imgIndex = [...entries].indexOf(img);
+        lightbox.style.display ="flex";
+        console.log("shitter");
+       
 
-const modal = document.querySelector(".modal");
-const modalImg = document.querySelector(".modalImg");
-const modalTxt = document.querySelector(".modalTxt");
-const close = document.querySelector(".close");
-const entryBox = document.querySelectorAll(".entry");
-
-const prevBtn = document.querySelector(".prevBtn");
-const nextBtn = document.querySelector(".nextBtn");
-
-let currentIndex = 0;
-
-
-
-entryBox.forEach((index) => {
-
- 
-          
-  document.querySelectorAll(".entry").forEach(el => el.addEventListener("click", event => { const img = el.querySelector("img");
-          
-          
         
-          openModal();
-          getIndex(index);
-          console.log(index);
+            lightbox.style.opacity ="1";
 
-          
-
-          
-          
-        }
-      )
-    );
-  image.addEventListener("click", () => {
-    close.addEventListener("click", () => {
-      modal.classList.remove("appear");
+       
     });
-  });
+});
 
-  });
+*/
 
+close.addEventListener('click', () => {
+    lightbox.classList.remove("appear");
 
-function openModal(index){
+})
 
-  modal.classList.add("appear");
-  getIndex(index);
+arrLeft.addEventListener('click', () =>{
+    imgIndex --;
+    if(imgIndex < 0) {
+        imgIndex = images.length -1;
 
+    }
+    mainImg.src = images[imgIndex].src;
+    modalTxt.innerHTML = images[imgIndex].alt;
+});
 
-}
+arrRight.addEventListener('click', () => {
+    imgIndex ++;
+    if(imgIndex > images.length -1) {
+        imgIndex = 0;
+    }
+    mainImg.src = images[imgIndex].src;
+    modalTxt.innerHTML = images[imgIndex].alt;
+});
 
-
-
-
-
-
-  document.addEventListener('keydown', function(ev) {
+document.addEventListener('keydown', function(ev) {
 
     if(ev.keyCode === 39){
-      getIndex(currentIndex - 1);
-      console.log(currentIndex);
-
+        imgIndex ++;
+    if(imgIndex > images.length -1) {
+        imgIndex = 0;
+    }
+    mainImg.src = images[imgIndex].src;
+    modalTxt.innerHTML = images[imgIndex].alt;
       }
    
   
     else if(ev.keyCode === 37){
       
-      getIndex(currentIndex + 1);
-      console.log(currentIndex);
- 
+        imgIndex --;
+    if(imgIndex < 0) {
+        imgIndex = images.length -1;
+
     }
-
-   
-});
-
-
-
-
-
-
+    mainImg.src = images[imgIndex].src;
+    modalTxt.innerHTML = images[imgIndex].alt;
   
-images.forEach((image) => {
-  image.addEventListener("click", () => {
-    modalImg.src = image.src;
+    }
+    if (event.key === 'Escape') {
+        lightbox.classList.remove("appear");
+    }
    
-    modalTxt.innerHTML = image.alt;
-    modal.classList.add("appear");
-
-    
-
-    close.addEventListener("click", () => {
-      modal.classList.remove("appear");
-    });
-
-//    modal.addEventListener("click", () => {
- //       modal.classList.remove("appear");
-  //    });
-  
-
   });
-});
-
-
-
-
-
-
-function closeModal() {
-    modal.classList.remove("appear");
-  }
-
-
-
   
-//modal.addEventListener('click', function(event) {
-//    const isOutside = !event.target.closest('.modalImg img');
-//    if (isOutside) {
-//        closeModal();
-//      }
-//  })
-
-
-window.addEventListener('keydown', event => {
-  console.log(event);
-  if (event.key === 'Escape') {
-    closeModal();
-  }
-});
-
-
-*/
