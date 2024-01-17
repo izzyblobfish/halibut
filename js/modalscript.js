855dfd1cdc62152364e8d4ac29ceadbfe1970461
izzyblobfish <izzyblobfish@outlook.com>
2024-01-16 19:59:55 -0800

    images progress index

--- js/modalscript.js
+++ js/modalscript.js
@@ -8,6 +8,10 @@ const arrLeft = document.querySelector('.prevBtn');
 const arrRight = document.querySelector('.nextBtn');
 const close = document.querySelector('.close');
 const modalTxt = document.querySelector(".modalTxt");
+const currentEntryIndex = document.querySelector(".currentEntryIndex");
+const totalEntryIndex = document.querySelector(".totalEntryIndex");
+
+
 
 let imgIndex = 0;
 
@@ -20,6 +24,11 @@ entries.forEach(img => {
         imgIndex = [...entries].indexOf(img);
         mainImg.src = images[imgIndex].src;
         modalTxt.innerHTML = images[imgIndex].alt;
+
+        currentEntry =  imgIndex + 1 ;
+        currentEntryIndex.innerHTML = currentEntry;
+        totalEntryIndex.innerHTML = images.length;
+
         lightbox.classList.add("appear");
         
     });
@@ -53,44 +62,66 @@ close.addEventListener('click', () => {
 
 arrLeft.addEventListener('click', () =>{
     imgIndex --;
+    currentEntry --;
     if(imgIndex < 0) {
         imgIndex = images.length -1;
 
     }
+    if(currentEntry < 1) {
+        currentEntry = images.length;
+
+    }
     mainImg.src = images[imgIndex].src;
     modalTxt.innerHTML = images[imgIndex].alt;
+    currentEntryIndex.innerHTML = currentEntry;
 });
 
 arrRight.addEventListener('click', () => {
     imgIndex ++;
+    currentEntry ++;
     if(imgIndex > images.length -1) {
         imgIndex = 0;
     }
+    if(currentEntry > images.length ) {
+        currentEntry = 1;
+    }
     mainImg.src = images[imgIndex].src;
     modalTxt.innerHTML = images[imgIndex].alt;
+    currentEntryIndex.innerHTML = currentEntry;
 });
 
 document.addEventListener('keydown', function(ev) {
 
     if(ev.keyCode === 39){
         imgIndex ++;
+        currentEntry ++;
     if(imgIndex > images.length -1) {
         imgIndex = 0;
     }
+    if(currentEntry > images.length ) {
+        currentEntry = 1;
+    }
     mainImg.src = images[imgIndex].src;
     modalTxt.innerHTML = images[imgIndex].alt;
+    currentEntryIndex.innerHTML = currentEntry;
       }
    
   
     else if(ev.keyCode === 37){
       
         imgIndex --;
+        currentEntry --;
     if(imgIndex < 0) {
         imgIndex = images.length -1;
 
     }
+    if(currentEntry < 1) {
+        currentEntry = images.length;
+
+    }
     mainImg.src = images[imgIndex].src;
     modalTxt.innerHTML = images[imgIndex].alt;
+    currentEntryIndex.innerHTML = currentEntry;
   
     }
     if (event.key === 'Escape') {
